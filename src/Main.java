301df718c2bfaf57594d99603114467ca0971eba
Abraão Reis <abraao@emcs.cnt.br>
2026-04-08 14:01:07 -0300

    feat: first case: add fixed income

--- src/Main.java
+++ src/Main.java
@@ -23,5 +23,49 @@ public class Main {
 
         FinancialGoal goal = new FinancialGoal(description, targetValue, deadline);
         Wallet wallet = new Wallet(goal);
+        StatementService statementService = new StatementService();
+
+        boolean running = true;
+
+        while (running) {
+            System.out.println("\n=== Menu Principal ===");
+            System.out.println("""
+                    1. Adicionar Renda Fixa (CDB, Tesouro Direto)
+                    2. Adicionar Renda Variável (Ações, FIIs)
+                    3. Ver Extrato da Carteira
+                    4. Buscar Investimento por Nome
+                    0. Sair da Aplicação
+                    """);
+
+            System.out.print("Escolha uma opção: ");
+            String option = input.nextLine();
+
+            switch (option) {
+                case "1":
+                    try {
+                        System.out.print("Informe o nome do ativo: ");
+                        String name = input.nextLine();
+
+                        System.out.print("Informe o valor aplicado (R$): ");
+                        double amount = input.nextDouble();
+
+                        System.out.print("Informe a taxa de juros ao mês (% em decimal; ex.: 0.01): ");
+                        double rate = input.nextDouble();
+
+                        System.out.print("Tempo investido (meses): ");
+                        int months = input.nextInt();
+
+                        FixedIncome fixedIncome = new FixedIncome(name, amount, rate, months);
+                        wallet.addInvestment(fixedIncome);
+
+                        System.out.println("Renda Fixa adicionada com sucesso!");
+                    } catch (NumberFormatException e) {
+                        System.out.println("Erro: Por favor, digite apenas números válidos para os valores.");
+                    }
+                    break;
+
+                // case "2":
+            }
+        }
     }
 }
