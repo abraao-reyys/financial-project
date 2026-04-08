5ba9cf0ddb5610776109284e9ca2b4a043f3a1de
Abraão Reis <abraao@emcs.cnt.br>
2026-04-08 13:52:12 -0300

    feat: main started; extractation of values and goal+wallet instance

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import model.*;
+import service.*;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Bem-vindo ao Gerenciador de Metas Financeiras!");
+        System.out.println("Primeiro, vamos definir o seu grande objetivo.");
+
+        System.out.print("Descreva brevemente a sua meta (ex.: Comprar Moto): ");
+        String description = input.nextLine();
+
+        System.out.print("Informe o valor alvo (R$): ");
+        double targetValue = input.nextDouble();
+
+        System.out.print("Informe o prazo da meta (ex: 02/2028): ");
+        String deadline = input.nextLine();
+
+        FinancialGoal goal = new FinancialGoal(description, targetValue, deadline);
+        Wallet wallet = new Wallet(goal);
     }
 }
